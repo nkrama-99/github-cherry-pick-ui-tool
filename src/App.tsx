fde921505caa0e634bd0da7f25ac5d6b80972530
Ramakrishna Natarajan <nkrama99@gmail.com>
2023-11-30 21:29:23 -0500

    stepper base changes

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,65 @@ import "@fontsource/roboto/300.css";
 import "@fontsource/roboto/400.css";
 import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
-import { AppBar, Box, Container, Toolbar, Typography } from "@mui/material";
+import React from "react";
+import {
+  AppBar,
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  Link,
+  Paper,
+  Step,
+  StepLabel,
+  Stepper,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import AddressForm from "./components/AddressForm";
+
+function Copyright() {
+  return (
+    <Typography variant="body2" color="text.secondary" align="center">
+      {"Copyright © "}
+      <Link color="inherit" href="https://mui.com/">
+        Your Website
+      </Link>{" "}
+      {new Date().getFullYear()}
+      {"."}
+    </Typography>
+  );
+}
+
+const steps = ["Shipping address", "Payment details", "Review your order"];
+
+function getStepContent(step: number) {
+  switch (step) {
+    case 0:
+      return <AddressForm />;
+    case 1:
+      return <AddressForm />;
+    case 2:
+      return <AddressForm />;
+    default:
+      throw new Error("Unknown step");
+  }
+}
 
 function App() {
+  const [activeStep, setActiveStep] = React.useState(0);
+
+  const handleNext = () => {
+    setActiveStep(activeStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep(activeStep - 1);
+  };
+
   return (
-    <main>
+    <React.Fragment>
+      <CssBaseline />
       <AppBar
         position="absolute"
         color="default"
@@ -17,14 +71,61 @@ function App() {
           borderBottom: (t) => `1px solid ${t.palette.divider}`,
         }}
       >
-        <Toolbar>Github Cherry-Pick Tool</Toolbar>
+        <Toolbar>
+          <Typography variant="h6" color="inherit" noWrap>
+            Company name
+          </Typography>
+        </Toolbar>
       </AppBar>
-      <Container component="main">
-        <Box>
-          <Typography>Let's Cherry Pick</Typography>
-        </Box>
+      <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
+        <Paper
+          variant="outlined"
+          sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
+        >
+          <Typography component="h1" variant="h4" align="center">
+            Checkout
+          </Typography>
+          <Stepper activeStep={activeStep} sx={{ pt: 3, pb: 5 }}>
+            {steps.map((label) => (
+              <Step key={label}>
+                <StepLabel>{label}</StepLabel>
+              </Step>
+            ))}
+          </Stepper>
+          {activeStep === steps.length ? (
+            <React.Fragment>
+              <Typography variant="h5" gutterBottom>
+                Thank you for your order.
+              </Typography>
+              <Typography variant="subtitle1">
+                Your order number is #2001539. We have emailed your order
+                confirmation, and will send you an update when your order has
+                shipped.
+              </Typography>
+            </React.Fragment>
+          ) : (
+            <React.Fragment>
+              {getStepContent(activeStep)}
+              <Box sx={{ display: "flex", justifyContent: "flex-end" }}>
+                {activeStep !== 0 && (
+                  <Button onClick={handleBack} sx={{ mt: 3, ml: 1 }}>
+                    Back
+                  </Button>
+                )}
+                <Button
+                  variant="contained"
+                  onClick={handleNext}
+                  sx={{ mt: 3, ml: 1 }}
+                >
+                  {activeStep === steps.length - 1 ? "Place order" : "Next"}
+                </Button>
+              </Box>
+            </React.Fragment>
+          )}
+        </Paper>
+        <Copyright />
       </Container>
-    </main>
+    </React.Fragment>
   );
 }
 
