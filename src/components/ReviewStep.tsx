d042ed0e9d91dcac072c8109aa549dfc6a6ea9cf
Ramakrishna Natarajan <nkrama99@gmail.com>
2023-12-01 02:49:09 -0500

    list commits

--- src/components/ReviewStep.tsx
+++ src/components/ReviewStep.tsx
@@ -5,9 +5,17 @@ import {
   Grid,
   TextField,
   Button,
+  List,
+  ListItem,
+  ListItemText,
+  ListItemIcon,
+  Divider,
 } from "@mui/material";
 import { FC, useEffect, useState } from "react";
 import { getCommitsInPR, Commit } from "../helper/OctokitHelper";
+import CommitIcon from "@mui/icons-material/Commit";
+
+const MAX_COMMIT_ID_LEN = 6;
 
 interface ReviewStepProps {
   githubToken: string;
@@ -16,6 +24,20 @@ interface ReviewStepProps {
   repo: string;
   pr: number;
 }
+
+const CommitListItem = (item: Commit, index: number) => {
+  return (
+    <ListItem key={index} divider>
+      <ListItemIcon>
+        <CommitIcon />
+      </ListItemIcon>
+      <ListItemText
+        primary={item.Message}
+        secondary={item.Id.slice(0, MAX_COMMIT_ID_LEN)}
+      ></ListItemText>
+    </ListItem>
+  );
+};
 
 const ReviewStep: FC<ReviewStepProps> = ({
   githubToken,
@@ -37,6 +59,7 @@ const ReviewStep: FC<ReviewStepProps> = ({
 
   const onClickCherryPick = () => {
     console.log("commits:", commits);
+    // nextStage();
   };
 
   return (
@@ -46,17 +69,13 @@ const ReviewStep: FC<ReviewStepProps> = ({
         sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}
       >
         <Typography component="h1" variant="h4" align="center" gutterBottom>
-          Review
+          Review Commits
         </Typography>
         <Grid container spacing={3}>
           <Grid item xs={12} sm={6}>
-            <TextField
-              required
-              label="Owner"
-              fullWidth
-              autoComplete="github-cherry-pick-tool-owner"
-              variant="standard"
-            />
+            <List>
+              {commits.map((item, index) => CommitListItem(item, index))}
+            </List>
           </Grid>
           <Grid item xs={12} sm={6}>
             <TextField
